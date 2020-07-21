76819ade06fe6c2245b38999b962e26dded3edb6
Enkhlen Khurelbaatar <enkhulek@gmail.com>
2020-07-20 18:23:58 -0700

    Added layout, refactored code by creating date module, added new todo list category 'Work'

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
 
 const app = express();
 
-var items = ["Morning Meditation", "Breakfast", "Exercise"];
+const items = ["Morning Meditation", "Breakfast", "Exercise"];
+const workItems = [];
 
 app.set('view engine','ejs');
 
@@ -14,23 +18,30 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    var today = new Date();
+    let day = date.getDate();
     
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-    res.render("list",{kindOfDay: day, newListItem: items});
+    res.render("list",{listTitle: day, newListItem: items});
    
 });
 
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItem: workItems });
+});
+
+
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+   let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
     
 });
 
